eb671259d9ebb7d67bae473d04a40c995d204906
andrejsim <andrej@knmi.nl>
2016-05-12 15:10:40 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='clipc_combine_process',
+      version='1.3',
+      description='CLIPC Combine utility used in PyWPS',
+      author='Andrej Mihajlovski, Maarten Plieger',
+      author_email='andrej.mihajlovski@knmi.nl, maarten.plieger@knmi.nl',
+      packages=['clipc_combine_process'],
+      )
